3b51533840fa239a018bf60bd67bb63ac9a6c926
Frotas <89676387+Frotas@users.noreply.github.com>
2023-11-22 15:30:41 -0300

    🎨Adição do 'Design System' do projeto ao Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,33 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        headline: 'Dosis, sans-serif',
+        title: 'Dosis, sans-serif',
+        subtitle: 'Lato, Exo, sans-serif',
+        display: 'Lato, Exo, sans-serif',
+        body: 'Lato, Exo, sans-serif',
+        label: 'Lato, Exo, sans-serif',
+        caption: 'Lato, Exo, sans-serif',
       },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
+      }
     },
+    colors: {
+      'Cyprus': '#0A192F',
+      'RockBlue': '#95A3BF',
+      'TrappedDarkness': '#081526',
+      'DarkSlateGray': '#1E5359',
+      'Black': '#0e0e0e',
+      'White': '#fefefe'
+    }
   },
   plugins: [],
 }
+
 export default config
