e7cfd0a53c883764c3503e6b2cf47eeefaf0d78a
frotas <89676387+Frotas@users.noreply.github.com>
2024-03-14 23:01:47 -0300

    FIX: "Cannot find module".

--- src/app/components/home/Form/Input/index.ts
+++
@@ -1,6 +0,0 @@
-import InputWrapper from "./InputRoot.component";
-import Input from "./Input.component";
-import Label from "./Label/Label.component";
-import Textarea from "./Textarea/Textarea.component";
-
-export { InputWrapper, Input, Label, Textarea };
