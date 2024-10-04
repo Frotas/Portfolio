522b751cb8cb23eb0842f5b8cf75091486a2084b
frotas <89676387+Frotas@users.noreply.github.com>
2024-10-04 16:55:44 -0300

    feat: The Navigation-Bar and ThemeProvider finished

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+export * from "@app/lib/utils/formatText";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
